d15d94c44870db819e0d85e2a1acd375c4a05133
Aleksandra <aleksandrainca@gmail.com>
2025-03-28 14:00:56 -0400

    With JSX in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+//Декларативный
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+//Императивный
 function App() {
   const [count, setCount] = useState(0)
-
+const year = new Date().getFullYear();
   return (
+	// Декларативный
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+		<div>{year}</div>
     </>
   )
 }
